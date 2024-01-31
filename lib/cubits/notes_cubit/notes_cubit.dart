af8251d1efc76c1a021e2388dfe3030868321a28
Taha Hamada <tahahamada2901@gmail.com>
2024-01-31 15:13:41 +0200

    create fetch noteMethods and adding BlocProvider To noteViews

--- lib/cubits/notes_cubit/notes_cubit.dart
+++ lib/cubits/notes_cubit/notes_cubit.dart
@@ -1,9 +1,23 @@
 import 'package:bloc/bloc.dart';
+import 'package:hive/hive.dart';
 import 'package:meta/meta.dart';
+import 'package:notes/constants.dart';
 import 'package:notes/models/note_model.dart';
 
 part 'notes_state.dart';
 
 class NotesCubit extends Cubit<NotesState> {
   NotesCubit() : super(NotesInitial());
+
+
+  fetchAllNotes() async{
+    //  emit(NotesLoadeding());
+    try {
+      var noteBox = Hive.box<NoteModel>(kNotesBox);
+     List<NoteModel> notes =  noteBox.values.toList();
+      emit(NotesSuccess(notes));
+    } on Exception catch (e) {
+      emit(NotesFailure(e.toString()));
+    }
+  }
 }
--- lib/views/notes_view.dart
+++ lib/views/notes_view.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:notes/cubits/notes_cubit/notes_cubit.dart';
 import 'package:notes/widgets/add_note_botton_sheet.dart';
 import 'package:notes/widgets/notes_view_body.dart';
 
@@ -7,20 +9,23 @@ class NotesView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          showModalBottomSheet(
-            isScrollControlled: true,
-            context: context,
-            builder: (context) {
-              return const AddNoteButtonSheet();
-            },
-          );
-        },
-        child: const Icon(Icons.add),
+    return BlocProvider(
+      create: (context) => NotesCubit(),
+      child: Scaffold(
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {
+            showModalBottomSheet(
+              isScrollControlled: true,
+              context: context,
+              builder: (context) {
+                return const AddNoteButtonSheet();
+              },
+            );
+          },
+          child: const Icon(Icons.add),
+        ),
+        body: const NotesviewBody(),
       ),
-      body: const NotesviewBody(),
     );
   }
 }
